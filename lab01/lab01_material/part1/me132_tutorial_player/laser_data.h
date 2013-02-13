af5a041a046bccc341182e3fb778d4aeb5407928
Shir Aharon <saharon@caltech.edu>
2013-02-13 05:43:30 -0500

    Modified laser data read in to use struct for each point

---
+++ lab01/lab01_material/part1/me132_tutorial_player/laser_data.h
@@ -0,0 +1,46 @@
+// This file contains a struct that holds laser scan data
+
+// Include guard
+#ifndef __LASER_DATA__
+#define __LASER_DATA__
+
+// Include namespaces for convenience in code writing
+using namespace std;
+
+// This object stores the (x,y) coordinates of two paired points
+struct LaserData
+{
+    double range;
+    double bearing;
+    LaserData() : range(0), bearing(0)  {}
+    LaserData(const double r, const double b) : range(r), bearing(b)    {}
+};
+// This function prints out a LaserData as [range, bearing]
+ostream& operator<<(ostream& os, const LaserData& ld)
+{
+    os << "[" << ld.range << ", " << ld.bearing << "]";
+    return os;
+}
+// This function reads in a LaserData given as [range, bearing]
+istream& operator>>(istream& s, LaserData& dp)
+{
+    double range = 0, bearing = 0;  // initialize components of vector to read in.
+    char ch = 0;                    // characters read in from string
+
+    if (!s)     {return s;}     // stream invalid, just return fail state
+
+    // Read in each component and return if invalid syntax occurs
+    s >> ch;
+    if (ch != '[') {s.clear(ios_base::failbit); return s;}  // no starting paren
+    s >> range >> ch;
+    if (ch != ',') {s.clear(ios_base::failbit); return s;}  // no ',' between num
+    s >> bearing >> ch;
+    if (ch != ']') {s.clear(ios_base::failbit); return s;}  // no closing paren
+
+    // Everything valid, create Color
+    dp = LaserData(range, bearing);
+
+    return s;
+}
+
+#endif // __LASER_DATA__
--- lab01/lab01_material/part1/me132_tutorial_player/me132_tutorial_1.cc
+++ lab01/lab01_material/part1/me132_tutorial_player/me132_tutorial_1.cc
@@ -5,6 +5,7 @@
 
 #include "cmdline_parsing.h"
 #include "common_functions.h"
+#include "laser_data.h"
 
 #include <iostream>
 #include <fstream>
@@ -22,64 +23,55 @@ int main(int argc, char **argv)
   ofstream data_file("data.txt");
 
   try {
-  	/* Initialize connection to player */
-	  PlayerClient robot(gHostname, gPort);
-	  Position2dProxy pp(&robot, gIndex);
-	  LaserProxy lp(&robot, gIndex); 
+    /* Initialize connection to player */
+        PlayerClient robot(gHostname, gPort);
+        Position2dProxy pp(&robot, gIndex);
+        LaserProxy lp(&robot, gIndex);
 
- 	  int num_attempts = 20;
-	  if(!check_robot_connection(robot, pp, 20)) 
-	  	exit(-2);
-  
-      int num_steps = 0;
+        int num_attempts = 20;
+        if(!check_robot_connection(robot, pp, 20))
+            exit(-2);
 
-	  // Now we start the main processing loop
-	  while(1) {
-	    // read from the proxies; YOU MUST ALWAYS HAVE THIS LINE
-	    robot.Read();
-        
-	    // these next three lines illustrate how you query the laserproxy to gather 
-	    // the laser scanner data        
-	    unsigned int n = lp.GetCount();
-	  	vector<double> range_data(n);
-	  	vector<double> bearing_data(n);
-	    for(uint i=0; i<n; i++) {
+        int num_steps = 0;
 
-	      range_data[i] = lp.GetRange(i);
-	      bearing_data[i] = lp.GetBearing(i);
+        // Now we start the main processing loop
+        while(1) {
+            // read from the proxies; YOU MUST ALWAYS HAVE THIS LINE
+            robot.Read();
 
-	    }
-    
-	    // Now laser range data can be accessed as a double vector, e.g. range_data[i] 
-	    // and bearing_data[i].
+            // query the laserproxy to gather the laser scanner data
+            unsigned int n = lp.GetCount();
+            vector<LaserData> data(n);
+            for(uint i=0; i<n; i++) {
+                data[i] = LaserData(lp.GetRange(i), lp.GetBearing(i));
+            }
 
-	    if (num_steps%5 == 0) {
+            // Now laser range data can be accessed as a double vector, e.g. range_data[i]
+            // and bearing_data[i].
 
-	    	// Print out time and number of points
-	    	data_file << "time: " << num_steps << endl;
-	    	data_file << "points: " << range_data.size() << endl;
+            if (num_steps%5 == 0) {
+                // Print out time and number of points
+                data_file << "time: " << num_steps << endl;
+                data_file << "points: " << data.size() << endl;
 
-    		for (int j = 0; j < range_data.size(); j++) {
+                for (int j = 0; j < data.size(); j++) {
+                    // Print out range and bearing data
+                    data_file << data[j] << endl;
+                }
+            }
 
-    			// Print out range and bearing data
-    			data_file << "[" << range_data[j] << ", " << bearing_data[j] << "]" << endl;
+            // Next time step
+            num_steps++;
 
-    		}
-	    }
+            double turnrate = 0.1;
+            double speed = 0.1;
+            pp.SetSpeed(speed, turnrate);
+        }
 
-	    // Next time step
-	    num_steps++;
+    } catch(PlayerError e) {
+        write_error_details_and_exit(argv[0], e);
+    }
 
-	    double turnrate = 0.1;
-	    double speed = 0.1;
-	    pp.SetSpeed(speed, turnrate);
-    
-	  }
+    return 0;
 
-  } catch(PlayerError e) {
-  	write_error_details_and_exit(argv[0], e);
-  }
-
-  return 0;
-  
 }
