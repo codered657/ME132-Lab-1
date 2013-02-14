446fb543866f6b56f598995d73587ae7cdfca184
Shir Aharon <saharon@caltech.edu>
2013-02-14 03:43:58 -0500

    Added plotting for tutorial 3

---
+++ lab01/lab01_material/part1/me132_tutorial_camera/me132_tutorial_3_plot.m
@@ -0,0 +1,17 @@
+function [] = me132_tutorial_3_plot(filename)
+% This function takes in a data file of robot poses and laser scans and
+% plots them.
+
+% Read in data
+data = csvread(filename);
+x = data(:,1);
+y = data(:,2);
+z = data(:,3);
+r = data(:,4) / 255;
+g = data(:,5) / 255;
+b = data(:,4) / 255;
+
+% Plot room data
+figure;
+scatter3(x, y, z, 4, [r,g,b]);
+title('Room Points from Laser Scan Data')
